374b0b0a962b7afb53cd77a480adf2dcd1eb5923
Pirozhok Super <andrejkotara730@gmail.com>
2024-11-27 18:10:38 +1000

    Добавьте файлы проекта.

---
+++ WpfApp3/Window1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+
+        private void rb1_Checked(object sender, RoutedEventArgs e)
+        {
+            if(sender is RadioButton radioButton)
+            {
+                switch (radioButton.Name)
+                {
+                    case "rb1":
+                        imgDisplay.Source = new BitmapImage(new Uri("C:/Users/andre/Downloads/images/cat1.jpg"));
+                        break;
+                    case "rb2":
+                        imgDisplay.Source = new BitmapImage(new Uri("C:/Users/andre/Downloads/images/cat2.jpg"));
+                        break;
+                    case "rb3":
+                        imgDisplay.Source = new BitmapImage(new Uri("C:/Users/andre/Downloads/images/cat3.jpg"));
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp3/Window2.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png";
+            openFileDialog.Title = "Выберите изображение";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+                imgDisplay.Source = bitmap;
+            }
+        }
+    }
+}
